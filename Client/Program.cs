04544d97cf9e45b2fa4c9301cf59e1ecaf2a0107
Aleksey Syryh <asyryh@gmail.com>
2019-08-29 20:45:14 +0500

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new RequestSocket())
+            {
+                client.Connect("tcp://localhost:5555");
+                int i = 1;
+                while (i <= 100000000)
+                {
+                    var data = new byte[i];
+                    client.SendFrame(data);
+                    Console.WriteLine($"Send {i} bytes");
+                    i *= 10;
+                    client.ReceiveFrameBytes();
+                }
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using NetMQ;
+using NetMQ.Sockets;
+using System;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (var server = new ResponseSocket())
+            {
+                server.Bind("tcp://*:5555");
+                while (true)
+                {
+                    var data = server.ReceiveFrameBytes();
+                    Console.WriteLine($"Received {data.Length} bytes");
+                    server.SendFrame(new byte[1]);
+                }
+            }
+        }
+    }
+}
